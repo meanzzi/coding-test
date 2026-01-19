351bb698a9fffc8d8cd0b384c1a217bc074efc01
Minji <meanzi.kim@gmail.com>
2026-01-19 20:46:17 +0900

    Update 이진 변환 반복하기.cpp

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -1,74 +1,31 @@
-// #include <string>
-// #include <vector>
-// #include <algorithm>
-
-// using namespace std;
-
-// vector<int> solution(string s) {
-//     vector<int> answer;
-//     int change = 0, zeroSum = 0;
-    
-//     while(s != "1"){
-//         int zero = 0; 
-        
-//         for(int i=0; i<s.size(); i++){
-//             if(s[i] == '0') {
-//                 zero ++;
-//             }
-//         }
-//         zeroSum += zero;
-        
-//         int ten = s.size() - zero; // 0초기화 필요
-        
-//         string two = ""; //문자열 초기화
-        
-//         while(ten > 0){
-//             two += (ten % 2) + '0';
-//             ten = ten / 2;         
-//         }
-        
-//         reverse(two.begin(), two.end());
-//         change ++;
-//         s = two;
-//     }
-//     answer.push_back(change);
-//     answer.push_back(zeroSum);
-        
-//     return answer;
-// }
-
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-vector<int> solution(string s) {
-    int change = 0;
-    int zeroSum = 0;
-
-    while (s != "1") {
+vector<int> solution(string s) {  
+    int zeroSum = 0, count = 0;  
+    while(s != "1"){
         int zeroCount = 0;
-
-        // 1. 0의 개수 세기
-        for (char c : s) {
-            if (c == '0') zeroCount++;
+         // 제거할 0 개수
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '0') zeroCount ++;
         }
 
-        zeroSum += zeroCount;
+        int len = s.size() - zeroCount; // 0제거 후 길이
 
-        // 2. 0 제거 후 남은 길이
-        int len = s.size() - zeroCount;
-
-        // 3. 길이를 2진수로 변환 (문자열 생성 X)
-        string next = "";
-        while (len > 0) {
-            next = (len % 2 ? "1" : "0") + next;
+        //이진 변환 결과
+        string result = "";
+        while(len > 0){
+            if(len % 2 == 0) result += '0';
+            else result += '1';
             len /= 2;
         }
-
-        s = next;
-        change++;
+        reverse(result.begin(), result.end());
+        s = result;
+        
+        count ++;
+        zeroSum += zeroCount;
     }
-
-    return {change, zeroSum};
+    return {count, zeroSum};
 }
