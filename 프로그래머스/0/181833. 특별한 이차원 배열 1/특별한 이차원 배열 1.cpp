0764ef2690b3d493239206dd00e9ebc356db66bc
Minji <meanzi.kim@gmail.com>
2026-01-19 17:07:41 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 2.43 ms, Memory: 5.87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer(n, vector<int>(n,0)); 
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(i==j) answer[i][j]++; 
+        }
+    } 
+    return answer;
+}
