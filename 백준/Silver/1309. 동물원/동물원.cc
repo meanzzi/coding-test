51f34a59463fe8ac20406e54f3e38467b0cefe66
Minji <meanzi.kim@gmail.com>
2026-02-10 20:34:42 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int dp[100009];
+int main() {
+    int n;
+    cin >> n;
+
+    dp[0] = 1, dp[1] = 3;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
+    }
+
+    cout << dp[n];
+    return 0;
+ }
