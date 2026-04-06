18b5d91ded6bfd2713b11d7f984032b0b5a71d8c
Minji <meanzi.kim@gmail.com>
2026-04-06 22:26:08 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,8 +1,4 @@
 function solution(my_string) {
-    const answer = [];
-    for(let i=0; i<my_string.length; i++){
-        answer.push(my_string[i].toLowerCase()); //배열에서만 push 가능
-    }
-    answer.sort(); //문자 정렬
-    return answer.join(''); //문자열 반환
+   return my_string.toLowerCase().split('').sort().join('');
+    
 }
